97aeaf83f6d35ef56aaf12eee05136f7fea784a4
Agnibha-das-8200 <154665340+Agnibha-das-8200@users.noreply.github.com>
2024-01-14 14:41:32 +0530

    Add files via upload

--- Project20.java
+++ Project20.java
@@ -1,3 +1,10 @@
+/*15. Write a program in Java to convert a given time input
+ * into 24 hour format and display the output. (For e.g if 
+ * time input is 02:35 A.M. the output will be 02:35 A.M., 
+ * if time input is 02:35 P.M., the output will be 14:35 P.M.,
+ * if the time input is 14:35 P.M., the output will be 14:35 P.M.)*/
+
+  
 import java.util.*;
 import java.lang.*;
 public class Project20
@@ -15,27 +22,27 @@ public class Project20
         
         System.out.println("Enter AM/PM: "); //only AM/PM 
         ap = sc.next();
-        
-        if((hh > 0 && hh < 12) && (ap.equals("AM")))
+        //checks the input and declares the output
+        if((hh > 0 && hh < 12) && (ap.equalsIgnoreCase("AM")))
         { 
             System.out.println("Im here"); 
             System.out.println(hh+": "+mm+" "+ap); 
         }
-        else if((hh == 0) && (ap.equals("PM")))
+        else if((hh == 0) && (ap.equalsIgnoreCase("PM")))
         { 
             System.out.println("Invalid time format!"); 
         }
-        else if((hh > 0 && hh < 12) && (ap.equals("PM"))) 
+        else if((hh > 0 && hh < 12) && (ap.equalsIgnoreCase("PM"))) 
         {
             hh = hh + 12;
             System.out.println(hh+": "+mm+" "+ap);
         }
-        else if((hh == 12) && (ap.equals("AM")))
+        else if((hh == 12) && (ap.equalsIgnoreCase("AM")))
         { 
             hh = hh - 12;
             System.out.println(hh+": "+mm+" "+ap);
         }
-        else if((hh == 12) && (ap.equals("PM")))
+        else if((hh == 12) && (ap.equalsIgnoreCase("PM")))
         {
             System.out.println(hh+": "+mm+" "+ap);
         }
@@ -43,7 +50,7 @@ public class Project20
         { 
             System.out.println(hh+": "+mm+" "+ap); 
         }
-        else if((hh >= 12 && hh <= 23) && (ap.equals("AM"))) 
+        else if((hh >= 12 && hh <= 23) && (ap.equalsIgnoreCase("AM"))) 
         { 
             System.out.println("Invalid time format!");
         }
